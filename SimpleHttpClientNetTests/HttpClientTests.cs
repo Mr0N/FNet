9bb5e41e739ffd04ce7832b74b1c114bb2449db3
user <user@DESKTOP-2ERAVDP>
2024-12-26 05:54:15 +0200

    test

---
+++ SimpleHttpClientNetTests/HttpClientTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FXNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FXNet.Tests
+{
+    [TestClass()]
+    public class HttpClientTests
+    {
+        [TestMethod()]
+        public void CreateConnectionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void WriteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReadTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SendTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SendTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DisposeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void HttpClientTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
